d163649104f47c20d34115bc54986d6cbd5bbf36
Ko5t1C <kos@wanalike.fr>
2017-07-04 21:01:38 +0200

    Ajout licence GNU/GPL

--- README.md
+++ README.md
@@ -5,3 +5,33 @@ installation d'un serveur web (paquets lamp & git) pour rpi3
 wget https://github.com/Ko5t1C/rpi-setup/archive/master.zip ; unzip master.zip ; cd rpi-setup-master ; ./install.sh
 
 # n'hésitez pas a réagir a partager ce script il a pour but de faciliter l'installtion d'un serveur web pour raspberry pi (ubuntu mate armf) et pc (distributions basées sur ubuntu)
+
+#Licences GNU/GPL
+
+Ce programme est un logiciel gratuit: vous pouvez le redistribuer et / ou modifier
+	Sous les termes de la licence publique générale GNU publiée par
+	La Free Software Foundation, soit la version 3 de la licence, soit
+	(À votre choix) toute version ultérieure.
+
+	Ce programme est distribué dans l'espoir qu'il sera utile,
+	Mais SANS AUCUNE GARANTIE; Sans même la garantie implicite de
+	QUALITÉ MARCHANDE OU ADAPTATION À UN USAGE PARTICULIER. Voir le
+	GNU General Public License pour plus de détails.
+
+	Vous devriez avoir reçu une copie de la GNU General Public License
+	Avec ce programme. Sinon, voir <http://www.gnu.org/licenses/>
+	ou <https://www.gnu.org/licenses/gpl-3.0.txt/>.
+
+This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+    or <https://www.gnu.org/licenses/gpl-3.0.txt/>.
--- install.sh
+++ install.sh
@@ -14,7 +14,40 @@
 #            => https://www.wanalike.fr              #
 #            => https://kos.wanalike.fr              #
 #                                                    #
-######################################################                                              
+######################################################
+
+######################################################################################
+#                                                                                    #
+# Ce programme est un logiciel gratuit: vous pouvez le redistribuer et / ou modifier #
+#   Sous les termes de la licence publique générale GNU publiée par                  #
+#   La Free Software Foundation, soit la version 3 de la licence, soit               #
+#   toute version ultérieure.                                                        #
+#                                                                                    #
+#   Ce programme est distribué dans l'espoir qu'il sera utile,                       #
+#   Mais SANS AUCUNE GARANTIE; Sans même la garantie implicite de                    #
+#   QUALITÉ MARCHANDE OU ADAPTATION À UN USAGE PARTICULIER. Voir le                  #
+#   GNU General Public License pour plus de détails.                                 #
+#                                                                                    #
+#   Vous devriez avoir reçu une copie de la GNU General Public license               #
+#   Avec ce programme. Sinon, voir <http://www.gnu.org/licenses/>                    #
+#   ou <https://www.gnu.org/licenses/gpl-3.0.txt/>.                                  #
+#                                                                                    #
+#                                                                                    #
+# This program is free software: you can redistribute it and/or modify               #
+#     it under the terms of the GNU General Public License as published by           #
+#     the Free Software Foundation, either version 3 of the License, or              #
+#     any later version.                                                             #
+#                                                                                    #
+#     This program is distributed in the hope that it will be useful,                #
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of                 #
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                  #
+#     GNU General Public License for more details.                                   #
+#                                                                                    #
+#     You should have received a copy of the GNU General Public License              #
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>           #
+#     or <https://www.gnu.org/licenses/gpl-3.0.txt/>.                                #
+#                                                                                    #
+######################################################################################                                              
 #!/bin/bash
 
 #/* Variables */#
